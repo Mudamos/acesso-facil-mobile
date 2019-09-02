ed8db7e5da413ce0effd1da75e4c00bac102776c
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2019-09-02 10:02:23 -0300

    Improved unused-vars eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: "@react-native-community",
   rules: {
     quotes: ["error", "double"],
-    "no-unused-vars": "error",
+    "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
     "no-shadow": "off",
   },
 };
