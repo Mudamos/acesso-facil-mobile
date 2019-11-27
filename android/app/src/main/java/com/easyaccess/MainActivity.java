f489c782cd936e0c07a1e5f0950bbb22375118bd
Caio Yoshio Itiroko <caioitiroko@ymail.com>
2019-11-27 17:18:48 -0300

    Update splash screen (#12)
    This PR closes #9.
    
    It adds native splash screens with expected layout by designer.
    
    **PREVIEW**
    
    | OS        | Type           | Layout  |
    | ------------- |:-------------:| -----:|
    | Android      | Phone/Tablet      | <img width="266" alt="Screen Shot 2019-11-21 at 09 57 37" src="https://user-images.githubusercontent.com/33432383/69340344-0d622000-0c46-11ea-9b67-b486d56c946d.png"> |
    | iOS | iPhone      | ![Screen Shot 2019-11-21 at 09 30 45](https://user-images.githubusercontent.com/33432383/69340435-44383600-0c46-11ea-90e6-eeb265e61cda.png) |
    | iOS | iPad      | ![Screen Shot 2019-11-21 at 09 30 35](https://user-images.githubusercontent.com/33432383/69340458-50bc8e80-0c46-11ea-8a98-4440c6f0c8b6.png) |

--- android/app/src/main/java/com/easyaccess/MainActivity.java
+++ android/app/src/main/java/com/easyaccess/MainActivity.java
@@ -1,8 +1,10 @@
 package com.easyaccess;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
@@ -24,5 +26,11 @@ public class MainActivity extends ReactActivity {
          return new RNGestureHandlerEnabledRootView(MainActivity.this);
         }
       };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
     }
 }
