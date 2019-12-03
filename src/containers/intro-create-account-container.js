8e8af9d6121a546037244887ebbd2ba17ebee029
Caio Yoshio Itiroko <caio.itiroko@tagview.com.br>
2019-12-03 09:46:22 -0300

    Use screen constants

--- src/containers/intro-create-account-container.js
+++ src/containers/intro-create-account-container.js
@@ -1,11 +1,12 @@
 import { compose, pure, withHandlers } from "recompose";
 
 import IntroCreateAccount from "../components/intro-create-account";
+import { SCREENS } from "../models";
 
 const enhance = compose(
   withHandlers({
     onContinue: ({ navigation }) => () => {
-      navigation.navigate("CreateAccount");
+      navigation.navigate(SCREENS.CREATE_ACCOUNT);
     },
   }),
   pure,
